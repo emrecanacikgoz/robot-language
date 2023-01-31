ba17ff5b87240f887366d8872ad374ae2c936f34
ecacikgoz97 <ecacikgoz97@gmail.com>
2023-02-01 01:25:19 +0300

    main script added

---
+++ main.py
@@ -0,0 +1,13 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pytorch_lightning as pl
+
+from blind_robot.data import CalvinDataset
+
+ROOT = "/datasets/calvin/D/training"
+THRESHOLD = 100 # crop data for debugging
+data = CalvinDataset(root=ROOT, threshold=THRESHOLD)
+train_dataloader = DataLoader(data, batch_size=3, shuffle=False)
+train_features = next(iter(train_dataloader))
+
+# breakpoint()
