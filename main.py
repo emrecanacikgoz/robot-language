7ece1957a66c2db44d63dc4da104ab8fdf0ca8e2
ecacikgoz97 <ecacikgoz97@gmail.com>
2023-02-09 18:45:59 +0300

    hydra config added

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+import hydra
+from omegaconf import OmegaConf
 
-from blind_robot.data import CalvinDataset
+from blind_robot.data import CalvinDataset, CalvinDataModule
+from blind_robot.model import gpt
 
-ROOT = "/Users/emrecanacikgoz/Desktop/rl-project/calvin_debug_dataset/training"
-THRESHOLD = 100 # crop data for debugging
-data = CalvinDataset(root=ROOT, threshold=THRESHOLD)
-train_dataloader = DataLoader(data, batch_size=3, shuffle=False)
-train_features = next(iter(train_dataloader))
 
-breakpoint()
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(config):
+    config = OmegaConf.to_container(config)
+    config = pl.utilities.parsing.AttributeDict(config)
+    pl.seed_everything(config["seed"])
+    print(config)
+
+    datamodule = CalvinDataModule(
+        train_data_dir=config.data["train_data_dir"], 
+        val_data_dir=config.data["val_data_dir"], 
+        keys=config.data["keys"],
+        batch_size=config.data["batch_size"], 
+        num_workers=config.data["num_workers"], 
+        pin_memory=config.data["pin_memory"],
+        )
+    datamodule.setup(stage='fit')
+
+    model = gpt(config=config)
+    # Initialize a trainer
+    trainer = pl.Trainer(
+        callbacks=[TQDMProgressBar(refresh_rate=20)],
+    )
+
+    # Train the model ⚡
+    #trainer.fit(mnist_model, train_loader)
+
+if __name__ == "__main__":
+    main()
