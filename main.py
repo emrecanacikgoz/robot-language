b46d9e74cd8a137ce7e1b6d184d646f3a7ed3acc
ecacikgoz97 <ecacikgoz97@gmail.com>
2023-02-13 11:52:34 +0300

    trainer args added

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import hydra, os
+from omegaconf import OmegaConf
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks.progress import TQDMProgressBar
-import hydra
-from omegaconf import OmegaConf
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
+
 
 from blind_robot.data import CalvinDataset, CalvinDataModule
 from blind_robot.model import gpt
@@ -15,7 +18,8 @@ def main(config):
     config = pl.utilities.parsing.AttributeDict(config)
     pl.seed_everything(config["seed"])
     print(config)
-
+    print(config.model["deneme"])
+    breakpoint()
     datamodule = CalvinDataModule(
         train_data_dir=config.data["train_data_dir"], 
         val_data_dir=config.data["val_data_dir"], 
@@ -27,9 +31,31 @@ def main(config):
     datamodule.setup(stage='fit')
 
     model = gpt(config=config)
+
+    # Initialize a logger
+    if config.trainer["logger"] == "tensorboard":
+        logger = TensorBoardLogger(save_dir=os.getcwd(), version=1, name="lightning_logs")
+    elif config.trainer["logger"] == "wandb":
+        logger = WandbLogger(save_dir=os.getcwd(), version=1, name="lightning_logs")
+    else:
+        logger = False
+
     # Initialize a trainer
     trainer = pl.Trainer(
+        accelerator=config.trainer["accelerator"],
+        devices=config.trainer["devices"],
+        max_epochs=config.trainer["max_epochs"],
+        max_steps=config.trainer["max_steps"],
+        gradient_clip_val=config.trainer["gradient_clip_val"],
+        precision=config.trainer["precision"],
+        accumulate_grad_batches=config.trainer["accumulate_grad_batches"],
+        check_val_every_n_epoch=config.trainer["check_val_every_n_epoch"],
+        auto_scale_batch_size=config.trainer["auto_scale_batch_size"],
+        auto_lr_find=config.trainer["auto_lr_find"],
+        strategy=config.trainer["strategy"],
+        resume_from_checkpoint=config.trainer["resume_from_checkpoint"],
         callbacks=[TQDMProgressBar(refresh_rate=20)],
+        logger=logger,
     )
 
     # Train the model ⚡
