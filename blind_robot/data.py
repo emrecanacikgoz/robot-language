ad8f5e75721e0c14dc8aab4d76818352d019f70c
ecacikgoz97 <ecacikgoz97@gmail.com>
2023-02-16 13:10:46 +0300

    update

--- blind_robot/data.py
+++ blind_robot/data.py
@@ -8,7 +8,7 @@ import pytorch_lightning as pl
 class CalvinDataset(Dataset):
     def __init__(
         self, 
-        root_data_dir, 
+        root_data_dir=None, 
         keys=["actions", "rel_actions", "robot_obs"]
         ):
         super().__init__()
@@ -65,71 +65,9 @@ class CalvinDataset(Dataset):
         if len(source) < self.max_episode_len:
             pads = [pad for _ in range(pad_size)]
             source = source + pads
-            return torch.tensor(source, dtype=torch.float)
+            return torch.tensor(np.array(source), dtype=torch.float)
         else:
-            return torch.tensor(source, dtype=torch.float)
+            return torch.tensor(np.array(source), dtype=torch.float)
     
     def _quantize(self, source):
         pass
-
-
-class CalvinDataModule(pl.LightningDataModule):
-    def __init__(
-        self, 
-        train_data_dir=None, 
-        val_data_dir=None,
-        keys=None, 
-        batch_size=8, 
-        num_workers=0, 
-        pin_memory=False
-        ):
-        super().__init__()
-
-        self.train_data_dir = train_data_dir
-        self.val_data_dir = val_data_dir
-        self.keys = keys
-        self.batch_size = batch_size
-        self.num_workers = num_workers
-        self.pin_memory = pin_memory
-
-    def setup(self, stage='fit'):
-        if stage == 'fit':
-            self.train_data = self.load_split(split='train')
-            self.val_data = self.load_split(split='val')
-        if stage == 'val' or stage == 'predict':
-            self.val_data = self.load_split(split='val')
-    
-    def load_split(self, split):
-        if split == 'train':
-            root_data_dir = self.train_data_dir
-        elif split == 'val':
-            root_data_dir = self.val_data_dir
-        return CalvinDataset(root_data_dir=root_data_dir, keys=self.keys)
-    
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_data,
-            batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=self.num_workers,
-            pin_memory=self.pin_memory,
-        )
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_data,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=self.num_workers,
-            pin_memory=self.pin_memory,
-        )
-
-    def predict_dataloader(self):
-        return DataLoader(
-            self.val_data,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=self.num_workers,
-            pin_memory=self.pin_memory,
-        )
-
