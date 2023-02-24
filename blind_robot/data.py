5dccbe0e39f29e88a5acabd54fc8ca272f774392
ecacikgoz97 <ecacikgoz97@gmail.com>
2023-02-24 12:38:20 +0300

    comments added

--- blind_robot/data.py
+++ blind_robot/data.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import Dataset
 
-class CalvinDataset(Dataset):
+class CalvinDataset_GPT(Dataset):
     def __init__(self, data=None, max_length=None, keys=None):
         super().__init__()
         self.data=data
@@ -13,26 +13,33 @@ class CalvinDataset(Dataset):
         self._load_data()
 
     def _load_data(self):
-        states = list()
         print(f"Loading the {self.data.split('/')[-1].split('-')[-1]} data from path {self.data}:")
+
+        # read data
+        states = list()
         with open(self.data, "r") as f:
             for line in tqdm(f):
-                l=line.rstrip("\n").split('\t')
-                id = l[0]
-                state = [float(i) for i in l[1:]]
-                k = {"actions": state[0:7],
-                    "rel_actions": state[7:14],
-                    "robot_obs_tcp_position": state[14:17],
-                    "robot_obs_tcp_orientation": state[17:20],
-                    "robot_obs_gripper_opening_width": state[20:21],
-                    "robot_obs_arm_joint_states": state[21:28],
-                    "gripper_action": state[28:29],
-                    "scene_obs": state[29:],
-                    }
-                state = list()
-                for key in self.keys:
-                    state.extend(k[key])
-                states.append(state)
+
+                # set data fields
+                l = line.rstrip("\n").split('\t')
+                _stateID, state = l[0], [float(i) for i in l[1:]]
+                fields = {"actions": state[0:7],
+                          "rel_actions": state[7:14],
+                          "robot_obs_tcp_position": state[14:17],
+                          "robot_obs_tcp_orientation": state[17:20],
+                          "robot_obs_gripper_opening_width": state[20:21],
+                          "robot_obs_arm_joint_states": state[21:28],
+                          "gripper_action": state[28:29],
+                          "scene_obs": state[29:],
+                         }
+                
+                # get only the desired fields in config.yaml
+                desired_state = list()
+                for desired_field in self.keys:
+                    desired_state.extend(fields[desired_field])
+                states.append(desired_state)
+
+        # set context length of gpt to max_length
         self.items = self._split_equal_lengths(states)
 
     def __len__(self):
@@ -40,21 +47,11 @@ class CalvinDataset(Dataset):
     
     def __getitem__(self, index):
         episode = self.items[index]
+        # convert tensor
         data = torch.tensor(np.array(episode), dtype=torch.float)
+        # set source and target for next-token prediction; i.e., auto-regressive decoder training
         x, y = data[:self.max_length], data[1:self.max_length+1]
         return (x, y)
-
-    def _get_pad_size(self, source):
-        return self.max_episode_len-len(source)
-    
-    def _pad_with_zeros(self, source, pad_size):
-        pad = abs(source[0]*0)
-        if len(source) < self.max_episode_len:
-            pads = [pad for _ in range(pad_size)]
-            source = source + pads
-            return torch.tensor(np.array(source), dtype=torch.float)
-        else:
-            return torch.tensor(np.array(source), dtype=torch.float)
     
     def _split_equal_lengths(self, states):
         print(f"Splitting states to {self.max_length} equal episodes:")
@@ -78,6 +75,7 @@ class CalvinDataset_MLP(Dataset):
         
         # load full data
         data = pd.read_csv(self.tsv_data, sep='\t', header=None)
+
         # load annotated %1 data
         annotations = np.load(f"{self.np_data}/lang_annotations/auto_lang_ann.npy", allow_pickle=True).item()
         
@@ -106,6 +104,8 @@ class CalvinDataset_MLP(Dataset):
                                 "language": annotation[1],
                                 "task": self._encode(annotation[2]),
                                 })
+            
+            # drop episodes shorter than 65 to avoid padding
             if len(episode) == 65:
                 self.items.append(episode)
 
@@ -115,10 +115,13 @@ class CalvinDataset_MLP(Dataset):
     def __getitem__(self, index):
         episode = self.items[index]
         x, y = [], []
+
+        # get only the desired fields in config.yaml
         for state in episode:
             state_actions = [state[key] for key in self.keys]
             x.append(np.concatenate(state_actions, axis=0))
             y.append(state["task"])
+
         return torch.tensor(np.array(x), dtype=torch.float), torch.tensor(np.array(y[0]), dtype=torch.long)
     
     def _encode(self, string):
