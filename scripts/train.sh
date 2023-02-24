efb1f8d3a39ba85b169fab2fb171c7c007f93a02
ecacikgoz97 <ecacikgoz97@gmail.com>
2023-02-24 11:11:50 +0300

    .

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,15 +3,15 @@
 #SBATCH --partition=ai
 #SBATCH --qos=ai 
 #SBATCH --account=ai 
-#SBATCH --cpus-per-task=6
+##SBATCH --cpus-per-task=6
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
 #SBATCH --gres=gpu:1
 ##SBATCH --constraint=rtx_a6000
 #SBATCH --constraint=tesla_t4
-#SBATCH --mem=120G 
+#SBATCH --mem=60G 
 #SBATCH --time=7-0:0:0
-#SBATCH --output=logs/gpt-2-gpu_%J.log
+#SBATCH --output=logs/mlp-2-gpu_%J.log
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
 #SBATCH --mail-user=eacikgoz17@ku.edu.tr
 
